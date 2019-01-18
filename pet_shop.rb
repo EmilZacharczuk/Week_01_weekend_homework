924e4e83e5cf2764164b4eb5b3403dc07062231d
EmilZacharczuk <emil.zacharczuk@yahoo.pl>
2019-01-18 16:31:10 +0000

    Removing pet

--- pet_shop.rb
+++ pet_shop.rb
@@ -49,3 +49,11 @@ def find_pet_by_name(shop, pet_name)
   end
   return nil
 end
+
+def remove_pet_by_name(shop, pet_name)
+  for pet in shop[:pets]
+    if pet[:name] == pet_name
+      shop[:pets].delete(pet)
+    end
+  end
+end
