0ab92bf929e6c86a1422d9ce93fa4b6a2dcc6e3d
EmilZacharczuk <emil.zacharczuk@yahoo.pl>
2019-01-18 15:55:57 +0000

    count of breeds.

--- pet_shop.rb
+++ pet_shop.rb
@@ -18,3 +18,34 @@ end
 def increase_pets_sold(shop, number_of_pets)
   return shop[:admin][:pets_sold] += number_of_pets
 end
+
+def stock_count(shop)
+  # count = shop[:pets].length
+  # return count
+  #
+  # or
+  count = 0
+  for pet in shop[:pets]
+    count += 1
+  end
+  return count
+end
+
+def pets_by_breed(shop, breed_name)
+  pets = []
+  for pet in shop[:pets]
+    if pet[:breed] == breed_name
+      pets.push(pet)
+    end
+  end
+  return pets
+end
+
+  # pets = []
+  # for pet in shop
+  #   if pet == breed_name
+  #     pets.add(pet)
+  #   end
+  # end
+  # return pets.length
+# end
